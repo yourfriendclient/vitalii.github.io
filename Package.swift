27ac115ba5b231044f86ab0b5fe21a86524fb987
yourfriendclient <134533485+yourfriendclient@users.noreply.github.com>
2024-09-15 21:54:04 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "VV",
+    platforms: [.macOS(.v11)],
+    dependencies: [
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0"),
+        .package(url: "https://github.com/swiftwasm/carton", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "VV",
+            dependencies: [
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]
+        ),
+    ]
+)
 
